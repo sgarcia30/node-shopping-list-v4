f68cc6a129ba5371dd1d70f61397f1b21d07d46c
Sarah Ann Garcia <garciatres@gmail.com>
2017-12-04 12:00:30 -0500

    added put functionality to the recipes

--- server.js
+++ server.js
@@ -111,6 +111,36 @@ app.delete('/recipes/:id', (req, res) => {
   res.status(204).end();
 });
 
+// when PUT request comes in with updated item, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `Recipes.update` with updated item.
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['recipeName', 'ingList', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating recipe list item \`${req.params.id}\``);
+  Recipes.update({
+    id: req.params.id,
+    recipeName: req.body.recipeName,
+    ingList: req.body.ingList
+  });
+  res.status(204).end();
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
